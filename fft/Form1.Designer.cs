134ba815cdc6769592fe9fd39a7bcfd84ab8dd2b
Левинский Иван <ivan.levinskiy@bk.ru>
2023-05-09 12:40:16 +0500

    20230509_1240 Настройка Anchor для элементов управления. Исправление общих ошибок

--- fft/Form1.Designer.cs
+++ fft/Form1.Designer.cs
@@ -37,7 +37,6 @@ namespace fft
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.trackBar_120Hz = new System.Windows.Forms.TrackBar();
             this.trackBar_180Hz = new System.Windows.Forms.TrackBar();
-            this.chartAmplitude = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label_rms_amplitude = new System.Windows.Forms.Label();
             this.label_rms_freq = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -47,13 +46,20 @@ namespace fft
             this.label4 = new System.Windows.Forms.Label();
             this.trackBar_240Hz = new System.Windows.Forms.TrackBar();
             this.chartSpectr = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.chartAmplitude = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar_120Hz)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar_180Hz)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartAmplitude)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar_60Hz)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar_240Hz)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartSpectr)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartAmplitude)).BeginInit();
             this.SuspendLayout();
             // 
             // trackBar_120Hz
@@ -62,7 +68,7 @@ namespace fft
             this.trackBar_120Hz.Location = new System.Drawing.Point(1083, 86);
             this.trackBar_120Hz.Margin = new System.Windows.Forms.Padding(4);
             this.trackBar_120Hz.Name = "trackBar_120Hz";
-            this.trackBar_120Hz.Size = new System.Drawing.Size(163, 45);
+            this.trackBar_120Hz.Size = new System.Drawing.Size(163, 56);
             this.trackBar_120Hz.TabIndex = 2;
             this.trackBar_120Hz.Scroll += new System.EventHandler(this.trackBar_120Hz_Scroll);
             // 
@@ -72,37 +78,9 @@ namespace fft
             this.trackBar_180Hz.Location = new System.Drawing.Point(1083, 149);
             this.trackBar_180Hz.Margin = new System.Windows.Forms.Padding(4);
             this.trackBar_180Hz.Name = "trackBar_180Hz";
-            this.trackBar_180Hz.Size = new System.Drawing.Size(163, 45);
+            this.trackBar_180Hz.Size = new System.Drawing.Size(163, 56);
             this.trackBar_180Hz.TabIndex = 3;
             this.trackBar_180Hz.Scroll += new System.EventHandler(this.trackBar_180Hz_Scroll);
-            // 
-            // chartAmplitude
-            // 
-            this.chartAmplitude.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea1.AxisY.Minimum = 0D;
-            chartArea1.AxisY2.Minimum = 0D;
-            chartArea1.Name = "ChartArea1";
-            this.chartAmplitude.ChartAreas.Add(chartArea1);
-            legend1.Enabled = false;
-            legend1.Name = "Legend1";
-            this.chartAmplitude.Legends.Add(legend1);
-            this.chartAmplitude.Location = new System.Drawing.Point(8, 14);
-            this.chartAmplitude.Margin = new System.Windows.Forms.Padding(4);
-            this.chartAmplitude.Name = "chartAmplitude";
-            series1.BorderWidth = 3;
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.Color = System.Drawing.Color.Red;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
-            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
-            this.chartAmplitude.Series.Add(series1);
-            this.chartAmplitude.Size = new System.Drawing.Size(1011, 327);
-            this.chartAmplitude.TabIndex = 1;
-            this.chartAmplitude.Text = "chart1";
             // 
             // label_rms_amplitude
             // 
@@ -111,7 +89,7 @@ namespace fft
             this.label_rms_amplitude.Location = new System.Drawing.Point(1053, 545);
             this.label_rms_amplitude.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_rms_amplitude.Name = "label_rms_amplitude";
-            this.label_rms_amplitude.Size = new System.Drawing.Size(111, 16);
+            this.label_rms_amplitude.Size = new System.Drawing.Size(113, 17);
             this.label_rms_amplitude.TabIndex = 5;
             this.label_rms_amplitude.Text = "СКЗ Амплитуды";
             // 
@@ -122,7 +100,7 @@ namespace fft
             this.label_rms_freq.Location = new System.Drawing.Point(1053, 594);
             this.label_rms_freq.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_rms_freq.Name = "label_rms_freq";
-            this.label_rms_freq.Size = new System.Drawing.Size(92, 16);
+            this.label_rms_freq.Size = new System.Drawing.Size(96, 17);
             this.label_rms_freq.TabIndex = 6;
             this.label_rms_freq.Text = "СКЗ Частоты";
             // 
@@ -133,7 +111,7 @@ namespace fft
             this.label1.Location = new System.Drawing.Point(1027, 87);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(48, 17);
             this.label1.TabIndex = 7;
             this.label1.Text = "120Гц";
             // 
@@ -144,7 +122,7 @@ namespace fft
             this.label2.Location = new System.Drawing.Point(1027, 160);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.Size = new System.Drawing.Size(48, 17);
             this.label2.TabIndex = 8;
             this.label2.Text = "180Гц";
             // 
@@ -155,7 +133,7 @@ namespace fft
             this.label3.Location = new System.Drawing.Point(1027, 25);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 16);
+            this.label3.Size = new System.Drawing.Size(40, 17);
             this.label3.TabIndex = 10;
             this.label3.Text = "60Гц";
             // 
@@ -165,7 +143,7 @@ namespace fft
             this.trackBar_60Hz.Location = new System.Drawing.Point(1083, 23);
             this.trackBar_60Hz.Margin = new System.Windows.Forms.Padding(4);
             this.trackBar_60Hz.Name = "trackBar_60Hz";
-            this.trackBar_60Hz.Size = new System.Drawing.Size(163, 45);
+            this.trackBar_60Hz.Size = new System.Drawing.Size(163, 56);
             this.trackBar_60Hz.TabIndex = 9;
             this.trackBar_60Hz.Value = 10;
             this.trackBar_60Hz.Scroll += new System.EventHandler(this.trackBar_60Hz_Scroll);
@@ -177,7 +155,7 @@ namespace fft
             this.label4.Location = new System.Drawing.Point(1027, 223);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.Size = new System.Drawing.Size(48, 17);
             this.label4.TabIndex = 12;
             this.label4.Text = "240Гц";
             // 
@@ -187,7 +165,7 @@ namespace fft
             this.trackBar_240Hz.Location = new System.Drawing.Point(1083, 212);
             this.trackBar_240Hz.Margin = new System.Windows.Forms.Padding(4);
             this.trackBar_240Hz.Name = "trackBar_240Hz";
-            this.trackBar_240Hz.Size = new System.Drawing.Size(163, 45);
+            this.trackBar_240Hz.Size = new System.Drawing.Size(163, 56);
             this.trackBar_240Hz.TabIndex = 11;
             this.trackBar_240Hz.Scroll += new System.EventHandler(this.trackBar_240Hz_Scroll);
             // 
@@ -196,40 +174,100 @@ namespace fft
             this.chartSpectr.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea2.Name = "ChartArea1";
-            this.chartSpectr.ChartAreas.Add(chartArea2);
-            legend2.Enabled = false;
-            legend2.Name = "Legend1";
-            this.chartSpectr.Legends.Add(legend2);
-            this.chartSpectr.Location = new System.Drawing.Point(8, 349);
+            chartArea1.AxisX.Title = "Частота, Гц";
+            chartArea1.AxisY.Title = "Магнитуда, дБ";
+            chartArea1.Name = "ChartArea1";
+            this.chartSpectr.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chartSpectr.Legends.Add(legend1);
+            this.chartSpectr.Location = new System.Drawing.Point(4, 22);
             this.chartSpectr.Margin = new System.Windows.Forms.Padding(4);
             this.chartSpectr.Name = "chartSpectr";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chartSpectr.Series.Add(series2);
-            this.chartSpectr.Size = new System.Drawing.Size(1011, 346);
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartSpectr.Series.Add(series1);
+            this.chartSpectr.Size = new System.Drawing.Size(1011, 322);
             this.chartSpectr.TabIndex = 1;
             this.chartSpectr.Text = "chart2";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(1, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.chartAmplitude);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label6);
+            this.splitContainer1.Panel2.Controls.Add(this.chartSpectr);
+            this.splitContainer1.Size = new System.Drawing.Size(1019, 704);
+            this.splitContainer1.SplitterDistance = 352;
+            this.splitContainer1.TabIndex = 13;
+            // 
+            // chartAmplitude
+            // 
+            this.chartAmplitude.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.AxisX.Title = "Сэмплы";
+            chartArea2.AxisY.Title = "Амплитуда, дБ";
+            chartArea2.AxisY2.Minimum = 0D;
+            chartArea2.Name = "ChartArea1";
+            this.chartAmplitude.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chartAmplitude.Legends.Add(legend2);
+            this.chartAmplitude.Location = new System.Drawing.Point(4, 25);
+            this.chartAmplitude.Margin = new System.Windows.Forms.Padding(4);
+            this.chartAmplitude.Name = "chartAmplitude";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Red;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.chartAmplitude.Series.Add(series2);
+            this.chartAmplitude.Size = new System.Drawing.Size(1011, 323);
+            this.chartAmplitude.TabIndex = 1;
+            this.chartAmplitude.Text = "chart1";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(1057, 346);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(4, 4);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(45, 16);
-            this.label5.TabIndex = 13;
-            this.label5.Text = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Амплитудный сигнал:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 1);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 17);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Спектр сигнала:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1261, 706);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.chartSpectr);
-            this.Controls.Add(this.chartAmplitude);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.trackBar_240Hz);
             this.Controls.Add(this.label3);
@@ -242,14 +280,19 @@ namespace fft
             this.Controls.Add(this.trackBar_120Hz);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Разложение амплитудного сигнала на спектры";
             ((System.ComponentModel.ISupportInitialize)(this.trackBar_120Hz)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar_180Hz)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartAmplitude)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar_60Hz)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar_240Hz)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartSpectr)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartAmplitude)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -258,7 +301,6 @@ namespace fft
         #endregion
         private System.Windows.Forms.TrackBar trackBar_120Hz;
         private System.Windows.Forms.TrackBar trackBar_180Hz;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chartAmplitude;
         private System.Windows.Forms.Label label_rms_amplitude;
         private System.Windows.Forms.Label label_rms_freq;
         private System.Windows.Forms.Label label1;
@@ -268,7 +310,10 @@ namespace fft
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TrackBar trackBar_240Hz;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartSpectr;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartAmplitude;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- fft/Form1.cs
+++ fft/Form1.cs
@@ -23,195 +23,18 @@ namespace fft
         List<int> buffer = new List<int>();
 
 
-        SerialPort sp;
-
-        AnalogInput analogInput = new AnalogInput();
-
-
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
             Draw();
-
-            //sp = new SerialPort();
-            //sp.BaudRate = 115200;
-            //sp.PortName = "COM4";
-            //sp.ReadTimeout = 5;
-            //sp.Open();
-
-            //SerialPortRead();
-
-            RTSerialCom.SerialClient sclient = new RTSerialCom.SerialClient("COM4", 115200);
-            sclient.OpenConn();
-            sclient.OnReceiving += Sclient_OnReceiving;
-
-            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
-            timer.Interval = 1000;
-            timer.Tick += Timer_Tick;
-            timer.Start();
         }
 
-        int counter = 0;
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            label5.Text = counter.ToString();
-            counter = 0;
-        }
-
-        private void Sclient_OnReceiving(object sender, RTSerialCom.DataStreamEventArgs e)
-        {
-            var buffer =  e.Response;
-            var values = analogInput.AddArray(buffer);
-
-            Action action = () =>
-            {
-                foreach (var value in values)
-                {
-                    //Добавление точки на график амплитуды
-                    chartAmplitude.Series[0].Points.AddXY(0, value);
-                    X++;
-
-                    counter++;
-                }
-
-                //Если на графике амплитуды точек больше 100 - удаляем самую первую
-                do
-                {
-                    chartAmplitude.Series[0].Points.RemoveAt(0);
-                }
-                while (chartAmplitude.Series[0].Points.Count > 1000);
-
-                //Костыль
-                for (int i = 0; i < chartAmplitude.Series[0].Points.Count; i++)
-                {
-                    chartAmplitude.Series[0].Points[i].XValue = i;
-                }
-
-                //Очистка графика спектра
-                chartSpectr.Series[0].Points.Clear();
-
-                //Если точек меньше 1000 - выход
-                if (chartAmplitude.Series[0].Points.Count < 1000)
-                {
-                    return;
-                }
-
-                Complex[] samplies = new Complex[numSamples];
-
-
-                for (int i = 0; i < numSamples; i++)
-                {
-                    samplies[i] = new Complex(chartAmplitude.Series[0].Points[i].YValues[0], 0);
-                }
-
-                Fourier.Forward(samplies, FourierOptions.NoScaling);
-
-
-                //Получаем спектр
-                for (int i = 0; i < samplies.Length / 2; i++)
-                {
-                    double mag = (2.0 / numSamples) * Math.Abs(Math.Sqrt(Math.Pow(samplies[i].Real, 2) + Math.Pow(samplies[i].Imaginary, 2)));
-                    double hzSample = sampleRate / numSamples;
-
-                    chartSpectr.Series[0].Points.AddXY(hzSample * i, mag);
-                }
-
-
-                Get_RMS_A();
-                Get_RMS_F();
-            };
-
-            this.Invoke(action);
-        }
-
-        void SerialPortRead()
-        {
-            Task.Factory.StartNew(() =>
-            {
-                while (true)
-                {
-
-
-                    if (sp.BytesToRead > 0)
-                    {
-
-                        byte bt = (byte)sp.ReadByte();
-                        var value = analogInput.Add(bt);
-
-                        if (value != null)
-                        {
-                            buffer.Add((int)value);
-
-                            if (buffer.Count > 1000)
-                            {
-                                buffer.RemoveAt(0);
-                            }
-
-                            Action action = () =>
-                            {
-                                DrawOnline((int)value);
-                            };
-
-                            this.Invoke(action);
-                        }
-                    }
-                }
-            });
-        }
-
-        Int16 X = 0;
-        void DrawOnline(int value)
-        {
-            //Добавление точки на график амплитуды
-            chartAmplitude.Series[0].Points.AddXY(0, value);
-            X++;
-
-            //Если на графике амплитуды точек больше 100 - удаляем самую первую
-            if (chartAmplitude.Series[0].Points.Count > 100)
-            {
-                chartAmplitude.Series[0].Points.RemoveAt(0);
-            }
-
-            //Костыль
-            for (int i = 0; i < chartAmplitude.Series[0].Points.Count; i++)
-            {
-                chartAmplitude.Series[0].Points[i].XValue = i;
-            }
-
-            //Очистка графика спектра
-            chartSpectr.Series[0].Points.Clear();
-
-            //Если точек меньше 1000 - выход
-            if (buffer.Count < 1000)
-            {
-                return;
-            }
-
-            Complex[] samplies = new Complex[numSamples];
-
-
-            for (int i = 0; i < numSamples; i++)
-            {
-                samplies[i] = new Complex(buffer[i], 0);
-            }
-
-            Fourier.Forward(samplies, FourierOptions.NoScaling);
-
-
-            //Получаем спектр
-            for (int i = 0; i < samplies.Length / 2; i++)
-            {
-                double mag = (2.0 / numSamples) * Math.Abs(Math.Sqrt(Math.Pow(samplies[i].Real, 2) + Math.Pow(samplies[i].Imaginary, 2)));
-                double hzSample = sampleRate / numSamples;
-
-                chartSpectr.Series[0].Points.AddXY(hzSample * i, mag);
-            }
-
-            Get_RMS_A();
-            Get_RMS_F();
-        }
-
+        /// <summary>
+        /// Метод для построения графиков
+        /// </summary>
         void Draw()
         {
             chartAmplitude.Series[0].Points.Clear();
@@ -299,33 +122,52 @@ namespace fft
             label_rms_freq.Text = $"СКЗ частоты:   {rms}";
         }
 
+        /// <summary>
+        /// Метод, вызываемый при перетаскивании 
+        /// ползунка 60Hz
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void trackBar_60Hz_Scroll(object sender, EventArgs e)
         {
             amplitude_60Hz = trackBar_60Hz.Value;
             Draw();
         }
 
+        /// <summary>
+        /// Метод, вызываемый при перетаскивании 
+        /// ползунка 120Hz
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void trackBar_120Hz_Scroll(object sender, EventArgs e)
         {
             amplitude_120Hz = trackBar_120Hz.Value;
             Draw();
         }
 
+        /// <summary>
+        /// Метод, вызываемый при перетаскивании 
+        /// ползунка 180Hz
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void trackBar_180Hz_Scroll(object sender, EventArgs e)
         {
             amplitude_180Hz = trackBar_180Hz.Value;
             Draw();
         }
 
+        /// <summary>
+        /// Метод, вызываемый при перетаскивании 
+        /// ползунка 240Hz
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void trackBar_240Hz_Scroll(object sender, EventArgs e)
         {
             amplitude_240Hz = trackBar_240Hz.Value;
             Draw();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
